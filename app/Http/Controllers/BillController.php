738eae44b68987ee8f63f0a4c5959e8b92ae4658
TuanAnh <anhptph30273@fpt.edu.vn>
2024-06-16 23:14:40 +0700

    Task API Bill, Cart
    Các Models cần có
    1. promotion
    2.user
    3.product_detail
    chờ để hoàn thiện nốt API

---
+++ app/Http/Controllers/BillController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Bills;
+use GuzzleHttp\Promise\Create;
+use App\Traits\MessageStatusAPI;
+
+class BillController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // Lấy ra danh sách Bills
+        $bill = Bills::all();
+        return MessageStatusAPI::list($bill, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // Validate check user và promotion không được null
+        // $request->validate([
+        //     'user_id' => 'required|integer',
+        //     'promotion_id ' => 'required|integer',
+        // ]);
+        $bill = Bills::create($request->all());
+        return MessageStatusAPI::store($bill, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $bill = Bills::findOrFail($id);
+        if ($bill == null) {
+            return MessageStatusAPI::notFound();
+        }
+        return MessageStatusAPI::show($bill, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        // $request->validate([
+        //     'user_id' => 'required|integer',
+        //     'promotion_id ' => 'required|integer',
+        // ]);
+
+        $Bill = Bills::findOrFail($id);
+        if ($Bill == null) {
+            return MessageStatusAPI::notFound();
+        }
+        $Bill->update($request->all());
+        return MessageStatusAPI::update($Bill, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $Bill = Bills::findOrFail($id);
+        if ($Bill == null) {
+            return MessageStatusAPI::notFound();
+        }
+        $Bill->delete();
+        return MessageStatusAPI::destroy(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\BillController;
+use App\Http\Controllers\BillDetailController;
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\PaymentController;
+use App\Models\Carts;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +22,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-//lấy toàn bộ thông tin thanh toán
+
+// Tuan Anh
+Route::apiResource('bill', BillController::class); // API bill
+Route::apiResource('billDetail', BillDetailController::class); // API bill detail
+Route::apiResource('cart', CartController::class);// API cart
